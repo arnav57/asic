9a90c5142ed54115a356a4c854ce69cf5b346cb0
Arnav Goyal <arnav.go.2003@gmail.com>
2026-03-12 01:45:31 -0400

    Pipe stage and dbg output on uart_top, add qpf, qsf, sdc for uart_top

--- design/uart/uart_top.sv
+++ design/uart/uart_top.sv
@@ -21,24 +21,40 @@ module uart_top #(
 	output wire 		rx_data_valid_o,
 
 	// Configuration
-	input  wire 		uart_lbpk_en_i
+	input  wire 		uart_lbpk_en_i,
+
+	// Debug
+	output wire [8-1:0]	dbg_data_o
 );
 
 // Loopback here means RX sends its recieved data to TX
 // To pull this off:
 //   1. we connect tx_data_i to rx_data_o
 //   2. Connect the data_valid signals
+// We pipeline stuff here so we can meet timing
 
 wire     	tx_data_valid_int;
 wire [7:0] 	tx_data_int;
-wire     	rx_data_valid_int;
-wire [7:0] 	rx_data_int;
+wire		rx_data_valid_int;
+wire [7:0]  rx_data_int;
+logic [7:0] rx_data_r;
+logic 		rx_data_valid_r;
 
-assign rx_data_valid_o = rx_data_valid_int;
-assign rx_data_o = rx_data_int;
+assign rx_data_valid_o = rx_data_valid_r;
+assign rx_data_o = rx_data_r;
 
-assign tx_data_valid_int = (uart_lbpk_en_i) ? rx_data_valid_int : tx_data_valid_i;
-assign tx_data_int       = (uart_lbpk_en_i) ? rx_data_int       : tx_data_i      ;
+always_ff @(posedge uart_clk_i) begin
+	if(~uart_rstn_i) begin
+		rx_data_r <= 8'b0;
+		rx_data_valid_r <= 1'b0;
+	end else begin
+		rx_data_r       <= rx_data_int;
+		rx_data_valid_r <= rx_data_valid_int;
+	end
+end
+
+assign tx_data_valid_int = (uart_lbpk_en_i) ? rx_data_valid_r : tx_data_valid_i;
+assign tx_data_int       = (uart_lbpk_en_i) ? rx_data_r       : tx_data_i      ;
 
 uart_tx #(
 	.LOGIC_FREQ     (LOGIC_FREQ ),
@@ -65,5 +81,7 @@ uart_rx #(
 	.rx_data_o      (rx_data_int      ),
 	.rx_data_valid_o(rx_data_valid_int)
 );
+
+assign dbg_data_o [7:0] = tx_data_int;
 
 endmodule : uart_top
